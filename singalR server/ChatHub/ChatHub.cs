6875221da51fa5d80119ad18ef72dda09cee6463
arst <artem.startsev@gmail.com>
2017-12-25 15:53:08 +0300

    signalr server

---
+++ singalR server/ChatHub/ChatHub.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatHub
+{
+    public class ChatHub : Hub
+    {
+        private static Dictionary<string, string> inChat = new Dictionary<string, string>();
+
+        public void Register(string userName)
+        {
+            inChat.Add(userName, Context.ConnectionId);
+            Clients.AllExcept(Context.ConnectionId).userConnected(userName);
+        }
+
+        public string[] GetUsersOnline()
+        {
+            return inChat.Keys.ToArray();
+        }
+
+        public void SendPrivateMessage(string name, string message)
+        {
+            string connectionId = null;
+
+            if (inChat.TryGetValue(name, out connectionId)) {
+                if (!String.IsNullOrEmpty(connectionId))
+                {
+                    Clients.Client(connectionId).sendPrivateMessage(inChat.FirstOrDefault(c => c.Value == Context.ConnectionId).Key,message);
+                }
+            }
+        }
+
+        public void SendPublicMessage(string message)
+        {
+            Clients.All.sendMessage(inChat.FirstOrDefault(c => c.Value == Context.ConnectionId).Key, message);
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            var client = inChat.FirstOrDefault(c => c.Value == Context.ConnectionId);
+            inChat.Remove(client.Key);
+            Clients.AllExcept(Context.ConnectionId).userDisconnected(client.Key);
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
---
+++ singalR server/ChatHub/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Hosting;
+using Owin;
+using Microsoft.Owin.Cors;
+
+namespace ChatHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This will *ONLY* bind to localhost, if you want to bind to all addresses
+            // use http://*:8080 to bind to all addresses. 
+            // See http://msdn.microsoft.com/en-us/library/system.net.httplistener.aspx 
+            // for more information.
+            string url = "http://localhost:8484";
+            using (WebApp.Start(url))
+            {
+                Console.WriteLine("Server running on {0}", url);
+                Console.ReadLine();
+            }
+        }
+    }
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
+        }
+    }
+}
